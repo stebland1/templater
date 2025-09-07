16422255363fefd85bd8dbe750f37777a78aa53c
Stephen Bland <stebland1@gmail.com>
2025-09-07 10:20:56 +0100

    chore(parser): header file update

--- include/parser.h
+++ include/parser.h
@@ -7,6 +7,11 @@
 
 #define MAX_FILE_PATH 64
 
+int is_opening_tag(char *p);
+int is_closing_tag(char *p);
+char *handle_scan(ParserContext *pctx, FileContext *fctx, char *p);
+char *handle_parse(ParserContext *pctx, FileContext *fctx, char *p);
+int flush_tag_to_output_buf(ParserContext *ctx, FileContext *fctx);
 int parse_file(ParserContext *pctx, FILE *fp);
 
 #endif
